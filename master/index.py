4fcdf9347c0f3ff2e253b00b0e4b5dddfdf124e1
claudiobzrr <claudio.bezerra1998@gmail.com>
2021-10-29 00:21:38 -0300

    comments add to improve changes implementation

--- master/index.py
+++ master/index.py
@@ -4,26 +4,47 @@ import os
 
 mainFolder = "C:/shared"
 
+
+# Função que lista os arquivos dentro da pasta "shared"
+def listFiles():
+
+    openList =  open('list.spd','w')
+
+    for file in os.listdir(mainFolder):
+        openList.write(file)
+        openList.write('\n')
+
+    openList.close()
+
+
+# Função que cria a pasta "shared" se a mesma não existir
 def createMainFolder():
     if not os.path.exists(mainFolder):
         os.mkdir(mainFolder)
 
+
+
+#função que verifica o aquivo "list.spd" e invoca a função de listagem dentro de si mesma
 def listWatcher():
     num = 1
     while(num>0):
 
         if(os.path.exists('list.spd')):
-            time.sleep(2)
-            print('Aguardando mudanças...')
+            time.sleep(10)
+            listFiles()
+            os.remove('list.spd')
 
         elif FileNotFoundError:
-            time.sleep(2)
             print('O arquivo list.spd não existe ou foi excluído')
-            print('Criando novo arquivo...')
+            print('Atualizando/ criando novo arquivo...')
             createFile = open('list.spd', 'w')
             createFile.close()
-            print('Arquivo criado')
+            listFiles()
+            print('Arquivo atualizado/criado')
 
 
+#Invocandoa  função que cria a pasta "shared"
 createMainFolder()
+
+#criando uma Thread e apontando o valor alvo para a função "listWatcher"
 threading.Thread(target=listWatcher).start()
