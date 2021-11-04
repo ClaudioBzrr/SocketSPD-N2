b9fdff7c98d1abe0237e4ae8538d6c4ed1a81a82
claudiobzrr <claudio.bezerra1998@gmail.com>
2021-11-03 21:59:16 -0300

    socket added

--- master/index.py
+++ master/index.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import os
+import socket
 
 mainFolder = ""
 master = ""
@@ -12,20 +13,19 @@ timesync = ""
 #Definindo as constantes do projeto que estão contidas dentro de "computers.spd", como a Pasta Shared por exemplo
 computers = open('computers.spd')
 for linha in computers:
-    
+
     if '[FOLDER]' in linha:
-        mainFolder =  computers.readline().rstrip( '\n' )
+        mainFolder =  computers.readline().rstrip('\n')
     elif '[MASTER]' in linha:
-        master =  computers.readline().rstrip( '\n' )
+        master =  computers.readline().rstrip('\n')
     elif '[PORT1]' in linha:
-        port1 =  computers.readline().rstrip( '\n' )
+        port1 =  computers.readline().rstrip('\n')
     elif '[PORT2]' in linha:
-        port2 =  computers.readline().rstrip( '\n' )
+        port2 =  computers.readline().rstrip('\n')
     elif '[TIMESYNC]' in linha:
-        timesync =  int(computers.readline().rstrip( '\n' ))/1000
+        timesync =  int(computers.readline().rstrip('\n'))/1000
 
 computers.close()
-
 
 
 
@@ -47,8 +47,7 @@ def createMainFolder():
         os.mkdir(mainFolder)
 
 
-
-#função que verifica o aquivo "list.spd" e invoca a função de listagem dentro de si mesma
+#Função que verifica o aquivo "list.spd" e invoca a função de listagem dentro de si mesma
 def listWatcher():
     num = 1
     while(num>0):
@@ -65,6 +64,7 @@ def listWatcher():
             createFile.close()
             listFiles()
             print('Arquivo atualizado/criado')
+
 
 
 #Invocando a função que cria a pasta "shared"
